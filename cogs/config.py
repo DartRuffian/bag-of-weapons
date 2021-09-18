3a96f9013097ba08bcd450ef4af835cad58fdd9c
DartRuffian <skulldude327@gmail.com>
2021-09-18 09:46:41 -0500

    Custom prefixes and add/remove default prefix when the bot joins a server

--- cogs/config.py
+++ cogs/config.py
@@ -31,17 +31,59 @@ class Config(commands.Cog, name="Configuration"):
             "crit_chance": 6,
             "hit_chance": 80,
         }
-        print(kwargs)
+        accepted_args = [
+            "crit_chance",
+            "hit_chance",
+            "fight_channel"
+        ]
+
         if kwargs == ("reset",):
             settings[str(ctx.guild.id)] = default_settings
         else:
             for argument in kwargs:
-                name, value = argument.split("=")
-                settings[str(ctx.guild.id)][name] = int(value)
+                if argument.lower() in accepted_args:
+                    name, value = argument.lower().split("=")
+                    settings[str(ctx.guild.id)][name] = int(value)
+                
+                else:
+                    await ctx.send(f"Whoops! Argument `{argument.lower()}` is an accept setting, the list of accepted settings are...")
+                    await ctx.send("\n".join(accepted_args))
         
         os.chdir(f"{self.bot.BASE_DIR}/resources")
         with open("settings.json", "w") as f:
             json.dump(settings, f, indent=2)
+        os.chdir(self.bot.BASE_DIR)
+    
+    @commands.command()
+    @has_permissions(manage_channels=True)
+    async def change_prefix(self, ctx, new_prefix):
+        os.chdir(f"{self.bot.BASE_DIR}/resources")
+        with open("prefixes.json", "f") as f:
+            prefixes = json.load(f)
+        prefixes[str(ctx.guild.id)] = new_prefix
+        with open("prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=2)
+        os.chdir(self.bot.BASE_DIR)
+    
+    # Add/Remove prefixes when joining and leaving a server
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        os.chdir(f"{self.bot.BASE_DIR}/resources")
+        with open("prefixes.json", "f") as f:
+            prefixes = json.load(f)
+        prefixes[str(guild.id)] = "-"
+        with open("prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=2)
+        os.chdir(self.bot.BASE_DIR)
+    
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        os.chdir(f"{self.bot.BASE_DIR}/resources")
+        with open("prefixes.json", "f") as f:
+            prefixes = json.load(f)
+        del prefixes[str(guild.id)]
+        with open("prefixes.json", "w") as f:
+            json.dump(prefixes, f, indent=2)
         os.chdir(self.bot.BASE_DIR)
 
 
